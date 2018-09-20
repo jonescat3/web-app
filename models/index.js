29c609a1d65ca32a4eb63ba2c18d531e192462a7
Trina Raye <jonescatrina1@gmail.com>
2018-09-20 19:22:31 -0400

    updates

---
+++ models/index.js
@@ -0,0 +1,22 @@
+// Exporting an object containing all of our models
+
+module.exports = {
+    Article: require("./Article"),
+    Note: require("./Note")
+  };
+  
+
+// Exporting axios
+
+module.exports = {
+    entry: './app.js',
+    output: {
+      filename: 'bundle.js'
+    },
+    externals: ['axios'],
+    resolve: {
+      alias: {
+        'js-data-angular': '../dist/js-data-angular.js'
+      }
+    }
+  };
